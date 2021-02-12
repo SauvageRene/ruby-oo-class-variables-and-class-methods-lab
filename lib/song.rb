34024eda9882913c203202eb0108a6a65c6e177f
Rene <renegarcia1864@gmail.com>
2021-02-12 11:24:25 -0600

    commit tree 1492f1b5faeab2a5ce40998541ba121a86b16168

--- lib/song.rb
+++ lib/song.rb
@@ -3,46 +3,55 @@ class Song
     @@count = 0
     @@artists = []
     @@genres = []
+
     def initialize(name, artist, genre)
-        @@count += 1
         self.name = name
         self.artist = artist
         self.genre = genre
-        @@artists << artist
-        @@genres << genre
+        @@genres << self.genre
+        @@artists << self.artist
+        @@count += 1
+    end
+
+    def self.genres
+        @@genres.unique
     end
 
     def self.count
         @@count
     end
+
     def self.artists
         @@artists.uniq
     end
+
     def self.genres
         @@genres.uniq
     end
+
     def self.genre_count
         hash = {}
-          @@genres.each do |genre|
+        @@genres.each do |genre|
             if hash[genre]
                 hash[genre] += 1
             else
                 hash[genre] = 1
             end
         end
-        hash
-        end
-        def self.artist_count
-            artists_hash = {}
-             @@artists.each do |artist|
-                if artists_hash[artist]
-                    artists_hash[artist] += 1
-                else
-                    artists_hash[artist] = 1
-                end
-            end
-            artists_hash
-        end
+        hash # we have to end the do and if statements first befor returning the hash
+    end
 
-       
+    def self.artist_count
+        hash = {}
+        @@artists.each do |artist|
+            if hash[artist]
+                hash[artist] += 1
+            else
+                hash[artist] = 1
+            end
+        end
+        hash
+    end
+
+
 end
